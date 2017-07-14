67b2a4bccf4e44ab1de975262e922327b6001958
hapfish <hapfish@vip.qq.com>
2017-07-14 18:36:28 +0800

    2017-07-14 lidongxu highill-practice-spark use JavaRDD map, flatMap method.

--- highill-practice-spark/src/main/java/com/highill/practice/spark/SparkContext.java
+++ highill-practice-spark/src/main/java/com/highill/practice/spark/SparkContext.java
@@ -1,7 +1,9 @@
 package com.highill.practice.spark;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -15,10 +17,10 @@ public class SparkContext {
 			SparkConf sparkConf = new SparkConf();
 			sparkConf.setAppName(appName);
 			sparkConf.setMaster(master);
-			
+
 			sparkContext = new JavaSparkContext(sparkConf);
 		}
-		
+
 		return sparkContext;
 	}
 
@@ -28,16 +30,31 @@ public class SparkContext {
 		// String master = "spark://127.0.0.1:7077";
 		String master = "local[*]";
 
-	
-
 		JavaSparkContext sparkContext = javaSparkContext(appName, master);
 
-		List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4);
 
 		JavaRDD<Integer> rddInteger = sparkContext.parallelize(integerList);
 		System.out.println("-----rddInteger: " + rddInteger);
 		Integer reduceResult = rddInteger.reduce((a, b) -> (a + b));
 		System.out.println("-----reduceResult: " + reduceResult);
+
+		System.out.println("-----rddInteger collect: " + rddInteger.collect());
+		System.out.println("-----rddInteger count: " + rddInteger.count());
+		System.out.println("-----rddInteger first: " + rddInteger.first());
+		System.out.println("-----rddInteger take 2: " + rddInteger.take(2));
+		System.out.println("-----rddInteger countByKey: " + rddInteger.countByValue());
+
+		System.out.println("-----rddInteger map: " + rddInteger.map(v -> (v.toString())).collect());
+		
+		JavaRDD<Integer> rddFlatMap = rddInteger.flatMap(v -> {
+			Set<Integer> set = new HashSet<Integer>();
+			set.add(v);
+			return set.iterator();
+		});
+		System.out.println("-----rddInteger flatMap: " + rddFlatMap.collect());
+		
+		
 
 		sparkContext.close();
 
